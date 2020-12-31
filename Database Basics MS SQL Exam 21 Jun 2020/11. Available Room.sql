11157f680308ee3bd164cb8d8cb7a53a30716a02
Georgi Vasilev <goshko09@gmail.com>
2020-12-31 17:23:00 +0200

    Removed one old file

--- Database Basics MS SQL Exam 21 Jun 2020/11. Available Room.sql
+++ Database Basics MS SQL Exam 21 Jun 2020/11. Available Room.sql
@@ -11,18 +11,45 @@ BEGIN
 										WHERE [Id] = @HotelId
 								 );
 	DECLARE @roomPrice INT = (
-								SELECT [Price] FROM Rooms
-									WHERE HotelId = @HotelId
+								SELECT TOP(1) [Price] FROM Rooms
+									WHERE [HotelId] = @HotelId AND [Beds] >= @People
+									ORDER BY [Price] DESC
 							 );
-	DECLARE @totalCost DECIMAL(18,2) = (@hotelBaseRate + @roomPrice) * @People;
+	DECLARE @totalPrice DECIMAL(18,2) = (@hotelBaseRate + @roomPrice) * @People;
 
-	IF((@Date >= (SELECT TOP(1) [ArrivalDate] FROM Trips WHERE [CancelDate] IS NOT NULL) 
-			 AND @Date <= (SELECT TOP(1) [ReturnDate] FROM Trips WHERE [CancelDate] IS NOT NULL)))
-	BEGIN
+	DECLARE @Availability INT = (
+									SELECT COUNT(*) FROM (SELECT [ArrivalDate], [ReturnDate] FROM Trips AS t
+															JOIN Rooms AS r ON r.[Id] = t.[RoomId]
+															WHERE ('2011-06-01' BETWEEN [ArrivalDate] AND [ReturnDate]) 
+															AND ([CancelDate] IS NOT NULL) AND (r.[HotelId] = @HotelId)) AS dateValidation
+								);
+
+	DECLARE @RoomCapacity INT = (
+									SELECT TOP(1) [Beds] FROM Rooms
+									WHERE [HotelId] = @HotelId AND [Beds] >= @People
+									ORDER BY [Price] DESC
+								);
+
+	IF(@Availability = 0 AND @RoomCapacity >= @People)
 		RETURN 'No rooms available';
-	END
 
-	RETURN 0;
+	DECLARE @GetRoomId INT = (
+								SELECT top(1) [Id] FROM Rooms WHERE [HotelId] = @HotelId AND [Beds] >= @People
+									ORDER BY [Price] DESC
+							 );
+	DECLARE @GetRoomType nvarchar(max) = (
+									SELECT [Type] FROM Rooms WHERE [Id] = @GetRoomId
+							   );
+	DECLARE @GetRoomBeds INT  = (
+									SELECT [Beds] FROM Rooms WHERE [Id] = @GetRoomId
+								);
+	DECLARE @Result VARCHAR(MAX) = CONCAT('Room ', cast(@GetRoomId as nvarchar(max)), ': ',
+										  @GetRoomType, ' ', cast(@GetRoomBeds as nvarchar(max)), ' beds', ' - ',
+										  cast(@totalPrice as nvarchar(max)));
+
+	RETURN @Result
 END
+		
+GO
 
-SELECT dbo.udf_GetAvailableRoom(94, '2015-07-26', 3)
+SELECT dbo.udf_GetAvailableRoom(19, '2011-06-01', 3)
